272c15585e79a79de4c5eb78ace839d4a301b242
Andri Fannar Kristjánsson <Andri@Hilti.is>
2024-02-08 22:08:34 +0000

    feat: SignUp
    Started work on Sign Up for Users.

---
+++ app/src/main/java/is/hi/afk6/hbv2/SignUpActivity.java
@@ -0,0 +1,18 @@
+package is.hi.afk6.hbv2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity
+{
+    //private ActivitySignUpBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+    }
+}
