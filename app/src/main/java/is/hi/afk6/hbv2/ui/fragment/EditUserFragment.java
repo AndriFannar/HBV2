27991e1ab5e5c74fc39ee767af539326d7394843
ahp9 <ahp9@hi.is>
2024-02-18 18:34:29 +0000

    Edit user komið 98%, getur farið fram og til baka og það stoppar ef þú ert með error, annars ferðu aftur á heimasíðuna

--- app/src/main/java/is/hi/afk6/hbv2/ui/fragment/EditUserFragment.java
+++ app/src/main/java/is/hi/afk6/hbv2/ui/fragment/EditUserFragment.java
@@ -22,11 +22,11 @@ import is.hi.afk6.hbv2.services.implementation.UserServiceImplementation;
 public class EditUserFragment extends Fragment {
     private FragmentEditUserBinding binding;
     private UserService userService;
+    private User user;
 
     @Override
     public  void onCreate(@Nullable Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-
         userService = new UserServiceImplementation(new APIServiceImplementation());
     }
 
@@ -36,7 +36,7 @@ public class EditUserFragment extends Fragment {
         binding = FragmentEditUserBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        User user = userService.getUserByID(8L);
+        user = userService.getUserByID(8L);
         edit_setup();
 
         if(user != null){
@@ -45,14 +45,17 @@ public class EditUserFragment extends Fragment {
             binding.editAddress.setText(user.getAddress());
             binding.editEmail.setText(user.getEmail());
         }
-        binding.buttonEditSumbit.setOnClickListener(v -> {
-            assert user != null;
-            validateUpdate(user);
-        });
+
+
+        binding.buttonEditSumbit.setOnClickListener(v -> validateUpdate());
 
         return view;
     }
 
+    /**
+     * Sets the error texts as invisible, so they don't show up
+     * when opening the edit screen.
+     */
     private void edit_setup(){
         binding.editErrorName.setVisibility(View.INVISIBLE);
         binding.editErrorPhoneNr.setVisibility(View.INVISIBLE);
@@ -60,7 +63,14 @@ public class EditUserFragment extends Fragment {
         binding.editErrorEmail.setVisibility(View.INVISIBLE);
     }
 
-    private void validateUpdate(User user){
+
+    /**
+     * Checks if the changes that are made are validated and allowed.
+     */
+    private void validateUpdate(){
+        if(user == null){
+            return;
+        }
         user.setName(binding.editName.getText().toString());
         user.setAddress(binding.editAddress.getText().toString());
         user.setPhoneNumber(binding.editPhone.getText().toString());
