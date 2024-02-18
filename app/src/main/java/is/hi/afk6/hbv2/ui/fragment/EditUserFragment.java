4dec240ca959f4752ba7d7a2fe3b051f05652d35
ahp9 <ahp9@hi.is>
2024-02-18 17:52:27 +0000

    Setja virkni að edita og error message en getur enn ekki farið til baka

--- app/src/main/java/is/hi/afk6/hbv2/ui/fragment/EditUserFragment.java
+++ app/src/main/java/is/hi/afk6/hbv2/ui/fragment/EditUserFragment.java
@@ -1,6 +1,112 @@
 package is.hi.afk6.hbv2.ui.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import is.hi.afk6.hbv2.R;
+import is.hi.afk6.hbv2.databinding.FragmentEditUserBinding;
+import is.hi.afk6.hbv2.entities.ErrorResponse;
+import is.hi.afk6.hbv2.entities.User;
+import is.hi.afk6.hbv2.networking.implementation.APIServiceImplementation;
+import is.hi.afk6.hbv2.services.UserService;
+import is.hi.afk6.hbv2.services.implementation.UserServiceImplementation;
 
 public class EditUserFragment extends Fragment {
+    private FragmentEditUserBinding binding;
+    private UserService userService;
+
+    @Override
+    public  void onCreate(@Nullable Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+
+        userService = new UserServiceImplementation(new APIServiceImplementation());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState ){
+        binding = FragmentEditUserBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        User user = userService.getUserByID(8L);
+        edit_setup();
+
+        if(user != null){
+            binding.editName.setText(user.getName());
+            binding.editPhone.setText(user.getPhoneNumber());
+            binding.editAddress.setText(user.getAddress());
+            binding.editEmail.setText(user.getEmail());
+        }
+        binding.buttonEditSumbit.setOnClickListener(v -> {
+            assert user != null;
+            validateUpdate(user);
+        });
+
+        return view;
+    }
+
+    private void edit_setup(){
+        binding.editErrorName.setVisibility(View.INVISIBLE);
+        binding.editErrorPhoneNr.setVisibility(View.INVISIBLE);
+        binding.editErrorAddress.setVisibility(View.INVISIBLE);
+        binding.editErrorEmail.setVisibility(View.INVISIBLE);
+    }
+
+    private void validateUpdate(User user){
+        user.setName(binding.editName.getText().toString());
+        user.setAddress(binding.editAddress.getText().toString());
+        user.setPhoneNumber(binding.editPhone.getText().toString());
+        user.setEmail(binding.editEmail.getText().toString());
+
+        ErrorResponse errorResponse = userService.updateUser(user.getId(), user);
+
+        if(errorResponse != null){
+            errorResponse_input(errorResponse);
+        } else {
+            FragmentManager fragmentManager = getParentFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            UserFragment userFragment = new UserFragment();
+            fragmentTransaction.replace(R.id.edit_fragment_container_view, userFragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    private void errorResponse_input(ErrorResponse errorResponse){
+        String nameError = errorResponse.getErrorDetails().get("name");
+        if (nameError != null)
+        {
+            binding.editErrorName.setText(nameError);
+            binding.editErrorName.setVisibility(View.VISIBLE);
+        }
+        String phoneError = errorResponse.getErrorDetails().get("phoneNumber");
+
+        if (phoneError != null)
+        {
+            binding.editErrorPhoneNr.setText(phoneError);
+            binding.editErrorPhoneNr.setVisibility(View.VISIBLE);
+        }
+        String addressError = errorResponse.getErrorDetails().get("address");
+
+        if (addressError != null)
+        {
+            binding.editErrorAddress.setText(addressError);
+            binding.editErrorAddress.setVisibility(View.VISIBLE);
+        }
+        String emailError = errorResponse.getErrorDetails().get("email");
+
+        if (emailError != null)
+        {
+            binding.editErrorEmail.setText(emailError);
+            binding.editErrorEmail.setVisibility(View.VISIBLE);
+        }
+
+    }
 }
--- app/src/main/java/is/hi/afk6/hbv2/ui/fragment/UserFragment.java
+++ app/src/main/java/is/hi/afk6/hbv2/ui/fragment/UserFragment.java
@@ -1,17 +1,18 @@
 package is.hi.afk6.hbv2.ui.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import is.hi.afk6.hbv2.R;
+import is.hi.afk6.hbv2.databinding.FragmentUserBinding;
 import is.hi.afk6.hbv2.entities.User;
 import is.hi.afk6.hbv2.networking.implementation.APIServiceImplementation;
 import is.hi.afk6.hbv2.services.UserService;
@@ -30,16 +31,26 @@ public class UserFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState ){
-        View view = inflater.inflate(R.layout.temp_user, container, false);
-
-        TextView users_name = view.findViewById(R.id.temp_name);
+        is.hi.afk6.hbv2.databinding.FragmentUserBinding binding = FragmentUserBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
         //Temp User Id
         User user = userService.getUserByID(8L);
 
         if(user != null){
-            users_name.setText(user.getName());
+            binding.userName.setText(user.getName());
+            binding.userPhoneNr.setText(user.getPhoneNumber());
+            binding.userAddress.setText(user.getAddress());
+            binding.userEmail.setText(user.getEmail());
         }
 
+
+        binding.buttonGoEdit.setOnClickListener(v -> {
+            FragmentManager fragmentManager = getParentFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            EditUserFragment editUserFragment = new EditUserFragment();
+            fragmentTransaction.replace(R.id.edit_fragment_container_view, editUserFragment);
+            fragmentTransaction.commit();
+        });
         return view;
     }
 }
