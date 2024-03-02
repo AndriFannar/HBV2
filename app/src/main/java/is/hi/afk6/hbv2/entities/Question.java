03089420a769ee6cc6c8dfd95854e4d693cdc668
Fridrik Olafsson <72094943+FridrikThor@users.noreply.github.com>
2024-03-02 18:13:32 +0000

    smá breytingar

--- app/src/main/java/is/hi/afk6/hbv2/entities/Question.java
+++ app/src/main/java/is/hi/afk6/hbv2/entities/Question.java
@@ -1,5 +1,10 @@
 package is.hi.afk6.hbv2.entities;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,14 +13,14 @@ import java.util.List;
  *
  * @author Andri Fannar Kristjánsson, afk6@hi.is
  * @since 07/01/2024
- * @version 1.0
+ * @version 1.5
  */
-public class Question
+public class Question implements Parcelable
 {
     private Long id;
     private String questionString;
     private double weight;
-    private List<Questionnaire> questionnaires;
+    private List<Long> questionnaireIDs;
     private Integer answer;
 
     /**
@@ -23,7 +28,7 @@ public class Question
      */
     public Question()
     {
-        this.questionnaires = new ArrayList<>();
+        this.questionnaireIDs = new ArrayList<>();
     }
 
     /**
@@ -36,7 +41,7 @@ public class Question
         this.questionString = questionString;
         this.weight = weight;
 
-        this.questionnaires = new ArrayList<>();
+        this.questionnaireIDs = new ArrayList<>();
     }
 
     /**
@@ -45,15 +50,29 @@ public class Question
      * @param id             Unique ID of Question.
      * @param questionString String for question to ask.
      * @param weight         Weight of the Question.
-     * @param questionnaires Questionnaires that Question belongs in.
+     * @param questionnaireIDs Questionnaires that Question belongs in.
      * @param answer         User selected answer.
      */
-    public Question(Long id, String questionString, double weight, List<Questionnaire> questionnaires, Integer answer) {
+    public Question(Long id, String questionString, double weight, List<Long> questionnaireIDs, Integer answer) {
         this.id = id;
         this.questionString = questionString;
         this.weight = weight;
-        this.questionnaires = questionnaires;
+        this.questionnaireIDs = questionnaireIDs;
         this.answer = answer;
+    }
+
+    /**
+     * Create a Question from Parcel.
+     *
+     * @param in Parcel input.
+     */
+    private Question (Parcel in)
+    {
+        this.id               = in.readLong();
+        this.questionString   = in.readString();
+        this.weight           = in.readDouble();
+        this.questionnaireIDs = in.readArrayList(Long.class.getClassLoader());
+        this.answer           = in.readInt();
     }
 
     public Long getId() {
@@ -80,12 +99,12 @@ public class Question
         this.weight = weight;
     }
 
-    public List<Questionnaire> getQuestionnaires() {
-        return questionnaires;
+    public List<Long> getQuestionnaireIDs() {
+        return questionnaireIDs;
     }
 
-    public void setQuestionnaires(List<Questionnaire> questionnaires) {
-        this.questionnaires = questionnaires;
+    public void setQuestionnaireIDs(List<Long> questionnaireIDs) {
+        this.questionnaireIDs = questionnaireIDs;
     }
 
     public Integer getAnswer() {
@@ -95,4 +114,32 @@ public class Question
     public void setAnswer(Integer answer) {
         this.answer = answer;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(questionString);
+        dest.writeDouble(weight);
+        dest.writeList(questionnaireIDs);
+        dest.writeInt(answer);
+    }
+
+
+    public static final Parcelable.Creator<Question> CREATOR = new Parcelable.Creator<Question>()
+    {
+        public Question createFromParcel(Parcel in)
+        {
+            return new Question(in);
+        }
+
+        public Question[] newArray(int size)
+        {
+            return new Question[size];
+        }
+    };
 }
