869683adee9f1305db1216afba6416e52325e8ec
Andri Fannar Kristjánsson <Andri@Hilti.is>
2024-03-03 18:46:56 +0000

    Added implementation for updating the WaitingListRequest

--- app/src/main/java/is/hi/afk6/hbv2/services/implementation/WaitingListServiceImplementation.java
+++ app/src/main/java/is/hi/afk6/hbv2/services/implementation/WaitingListServiceImplementation.java
@@ -1,11 +1,20 @@
 package is.hi.afk6.hbv2.services.implementation;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 import is.hi.afk6.hbv2.entities.Questionnaire;
 import is.hi.afk6.hbv2.entities.User;
 import is.hi.afk6.hbv2.entities.WaitingListRequest;
 import is.hi.afk6.hbv2.entities.api.APICallback;
+import is.hi.afk6.hbv2.entities.api.ResponseWrapper;
+import is.hi.afk6.hbv2.networking.APIService;
 import is.hi.afk6.hbv2.services.WaitingListService;
 
 /**
@@ -18,6 +27,14 @@ import is.hi.afk6.hbv2.services.WaitingListService;
  */
 public class WaitingListServiceImplementation implements WaitingListService
 {
+    private final APIService apiService;
+    private final Executor executor;
+
+    public WaitingListServiceImplementation(APIService apiService, Executor executor)
+    {
+        this.apiService = apiService;
+        this.executor = executor;
+    }
 
     @Override
     public void saveNewWaitingListRequest(WaitingListRequest request, APICallback<WaitingListRequest> callback) {
@@ -45,8 +62,38 @@ public class WaitingListServiceImplementation implements WaitingListService
     }
 
     @Override
-    public void updateWaitingListRequestByID(Long requestID, WaitingListRequest updatedRequest, APICallback<WaitingListRequest> callback) {
+    public void updateWaitingListRequestByID(Long requestID, WaitingListRequest updatedRequest, APICallback<WaitingListRequest> callback)
+    {
+        executor.execute(new Runnable() {
+            @Override
+            public void run()
+            {
+                try {
+                    // Convert User class to String.
+                    String requestJson = new Gson().toJson(updatedRequest);
 
+                    // Send JSON data to API, wait for a return.
+                    JSONObject returnJson = apiService.putRequest("waitingList/update/" + requestID, requestJson);
+
+                    if (returnJson != null && returnJson.length() > 0)
+                    {
+                        // If return is not empty, convert from JSON to ErrorResponse.
+                        Gson gson = new Gson();
+                        Type responseType = new TypeToken<ResponseWrapper<WaitingListRequest>>() {}.getType();
+
+                        callback.onComplete(gson.fromJson(returnJson.toString(), responseType));
+                    }
+                    else
+                    {
+                        callback.onComplete(new ResponseWrapper<WaitingListRequest>(updatedRequest));
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     @Override
